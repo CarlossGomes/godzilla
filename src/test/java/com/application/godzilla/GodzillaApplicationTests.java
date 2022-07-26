b4f38a0c071a70c3c54ba867dee4cf2c958a09d1
carlos.gomes <carlos.gomes@castgroup.com.br>
2022-07-26 14:56:58 -0300

    Initial Commit

---
+++ src/test/java/com/application/godzilla/GodzillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.godzilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodzillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
