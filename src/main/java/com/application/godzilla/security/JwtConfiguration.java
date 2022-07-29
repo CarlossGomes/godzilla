ab6573a9c0a6cc409e44a3ff97bc95ba07c7f90b
carlos.gomes <carlos.gomes@castgroup.com.br>
2022-07-29 13:06:42 -0300

    Security

--- src/main/java/com/application/godzilla/security/JwtConfiguration.java
+++ src/main/java/com/application/godzilla/security/JwtConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -55,6 +54,5 @@ public class JwtConfiguration {
 
         return http.build();
     }
-
 
 }
